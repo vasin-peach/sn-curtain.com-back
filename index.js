bf4014ffd636f23503cbc96e2f98cfc0f22ff742
Peaches <shinasaki07@gmail.com>
2018-06-09 18:09:07 +0700

    :bug: change port 3000 to 5000

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 
 // listen on port 3000
-app.listen(3000, function () {
-  console.log("Express app listening on port 3000");
+app.listen(5000, function () {
+  console.log("Express app listening on port 5000");
 });
