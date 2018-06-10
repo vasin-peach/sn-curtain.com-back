71b2dd6c5e026809a8ac7f9ff33f5611ca740b3d
Peaches <shinasaki07@gmail.com>
2018-06-10 18:04:04 +0700

    :art: update comment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-// listen on port 3000
+// listen on port 5000
 app.listen(5000, function () {
   console.log("Express app listening on port 5000");
 });
