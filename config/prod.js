8fe2bf062c2d1a0d8fad897ac9e82c760bff91e7
Peaches <shinasaki07@gmail.com>
2018-06-14 19:21:13 +0700

    :bug: fix

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 // Production config
 module.exports = {
-  MONGO_URI: process.env.MONGO_URI_PROD,
+  MONGO_URI: process.env.MONGO_URI,
   MONGO_USER: process.env.MONGO_USER,
   MONGO_PASS: process.env.MONGO_PASS,
 };
--- config/stag.js
+++ config/stag.js
@@ -1,6 +1,6 @@
 // Staging config
 module.exports = {
-  MONGO_URI: process.env.MONGO_URI_STAG,
+  MONGO_URI: process.env.MONGO_URI,
   MONGO_USER: process.env.MONGO_USER,
   MONGO_PASS: process.env.MONGO_PASS,
 };
