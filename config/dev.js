3d6e315a14ab979da195023e0fbba3f18a4db4d4
Peaches <shinasaki07@gmail.com>
2018-07-26 18:49:13 +0700

    :bug: fix passport.

--- config/dev.js
+++ config/dev.js
@@ -7,5 +7,8 @@ module.exports = {
   SESSION_SECRET: 'dev',
   FACEBOOK_SECRET: '1400322780261711',
   FACEBOOK_CLIENT: 'a7e72e986e4999a67b1e90d9fd6686ed',
-  FACEBOOK_CALLBACK: 'http://localhost:5000/auth/facebook/callback'
+  FACEBOOK_CALLBACK: 'http://localhost:5000/auth/facebook/callback',
+  GOOGLE_SECRET: '',
+  GOOGLE_CLIENT: '',
+  GOOGLE_CALLBACK: ''
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,15 +3,35 @@ import passport from 'passport';
 import User from '../../models/User';
 import keys from '../../config/keys';
 const FacebookStrategy = require('passport-facebook').Strategy;
+const GoogleStrategy = require('passport-google').Strategy;
 const router = express.Router();
 
+
+passport.serializeUser((user, done) => { // store data in session
+  done(null, user);
+})
+passport.deserializeUser(function (obj, done) { // get data from session
+  done(null, obj)
+})
+
+// Init facebook strategy
 passport.use(new FacebookStrategy({
   clientID: keys.FACEBOOK_CLIENT,
   clientSecret: keys.FACEBOOK_SECRET,
   callbackURL: keys.FACEBOOK_CALLBACK
 }, (accessToken, refreshToken, profile, done) => {
   console.log(profile);
-  done(null, profile)
+  done(null, profile);
+}))
+
+// Init google strategy
+passport.use(new GoogleStrategy({
+  clientID: keys.GOOGLE_CLIENT,
+  clientSecret: keys.GOOGLE_SECRET,
+  callbackURL: keys.GOOGLE_CALLBACK
+}, (accessToken, refreshToken, profile, done) => {
+  console.log(profile);
+  done(null, profile);
 }))
 
 
@@ -23,6 +43,12 @@ passport.use(new FacebookStrategy({
 router.get('/', (req, res) => res.send('auth api.'))
 router.get('/facebook', passport.authenticate('facebook'))
 router.get('/facebook/callback', passport.authenticate('facebook', {
+  successRedirect: '/auth/profile',
+  failureRedirect: '/auth/error'
+}))
+
+router.get('/google', passport.authenticate('google'))
+router.get('/google/callback', passport.authenticate('facebook', {
   successRedirect: '/auth/profile',
   failureRedirect: '/auth/error'
 }))
