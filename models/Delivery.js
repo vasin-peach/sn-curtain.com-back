c8d1536103c0f15cee2e53d2adbcc6eba8d26339
Peaches <shinasaki07@gmail.com>
2018-10-26 19:33:04 +0700

    :bug: wrong schema.

--- models/Delivery.js
+++ models/Delivery.js
@@ -2,110 +2,18 @@ const mongoose = require('mongoose');
 const {
   Schema
 } = mongoose;
-
-const billSchema = new Schema({
-
-  bill_id: String,
-  title: String,
-  description: String,
+const keys = require('../config/keys');
 
 
-
-  name: {
+// Schema
+const deliverySchema = new Schema({
+  text: {
     type: String
   },
-  desc: [{
-    lang: {
-      type: String
-    },
-    val: {
-      type: String
-    }
-  }],
-  price: [{
-    text: {
-      type: String
-    },
-    value: {
-      type: Number
-    },
-    option: {
-      type: String
-    }
-  }],
-  quantity: {
+  value: {
     type: Number
-  },
-  like: {
-    type: Number
-  },
-  view: {
-    type: Number
-  },
-  brand: {
-    src: {
-      type: String
-    }
-  },
-  assets: [{
-    name: {
-      type: String
-    },
-    src: {
-      type: String
-    }
-  }],
-  category: {
-    val: {
-      type: String
-    },
-    type: {
-      val: {
-        type: String
-      },
-      nature: [{
-        val: {
-          type: String
-        },
-        text: {
-          type: String
-        },
-        option: {
-          type: String
-        }
-      }]
-    }
-  },
-  tag: [{
-    type: String
-  }],
-  specs: [{
-    name: {
-      type: String
-    },
-    val: {
-      type: String
-    }
-  }],
-  date: {
-    type: Date,
-    default: Date.now
-  },
-  comments: [{
-    body: String,
-    date: Date
-  }],
-  promotion: {
-    expired: {
-      expired: Boolean,
-      expiredStart: Date,
-      expiredEnd: Date,
-    },
-    discount: {
-      percent: Number,
-      amount: Number
-    }
   }
 })
 
-module.exports = mongoose.model('Bill', billSchema);
+// Exports
+module.exports = mongoose.model('Delivery', deliverySchema)
